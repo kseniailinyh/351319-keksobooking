f7ba2c314a0765a3321a7039618dbc93e956ffcd
Ksenia Ilinyh <ksenia.ilinyh@gmail.com>
2017-02-02 21:49:50 +0500

    Технический комит

--- js/form.js
+++ js/form.js
@@ -3,27 +3,72 @@
 var pins = document.querySelectorAll('.pin');
 var dialog = document.querySelector('.dialog');
 var dialogClose = dialog.querySelector('.dialog__close');
+var ENTER_KEY_CODE = 13;
+var ESC_KEY_CODE = 27;
 
 (function initPinClicks() {
-  // навешиваем каждому пину событие по клику
+  // навешиваем каждому пину событие по клику и по нажатию enter
   for (var i = 0; i < pins.length; i++) {
     pins[i].addEventListener('click', function (pin) {
       return function () {
-        removeActivePin();
-        pin.classList.add('pin--active');
-        dialog.classList.remove('invisible');
+        initPinEvent(pin);
+      };
+    }(pins[i])
+    );
+
+    pins[i].addEventListener('keydown', function (pin, evt) {
+      return function () {
+        alert('ok');
+        if (isActivateEvent(evt)) {
+          initPinEvent(pin);
+        }
       };
     }(pins[i])
     );
   }
 })();
 
-// навешиваем событие по клику на крестик
-dialogClose.addEventListener('click', function () {
-  // по клику на крестик закрываем диалог, то есть добавляем класс .invisible
-  dialog.classList.add('invisible');
-  // и убираем активный пин
+function initPinEvent(activePin) {
   removeActivePin();
+  showDialog();
+  activePin.classList.add('pin--active');
+}
+
+// проверяем, было ли нажатие на enter
+function isActivateEvent(evt) {
+  return evt.keyCode === ENTER_KEY_CODE;
+}
+
+// если было нажатие на esc, прячем setup
+function setupKeydownHandler(evt) {
+  if (evt.keyCode === ESC_KEY_CODE) {
+    removeActivePin();
+    hideDialog();
+  }
+}
+
+function showDialog() {
+  dialog.classList.remove('invisible');
+  document.addEventListener('keydown', setupKeydownHandler);
+}
+
+function hideDialog() {
+  dialog.classList.add('invisible');
+  document.removeEventListener('keydown', setupKeydownHandler);
+}
+
+// навешиваем событие по клику на крестик
+dialogClose.addEventListener('click', function (evt) {
+  hideDialog();
+  removeActivePin();
+});
+
+// навешиваем событие по нажатию enter на крестик, когда он в фокусе
+dialogClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ESC_KEY_CODE) {
+    hideDialog();
+    removeActivePin();
+  }
 });
 
 function removeActivePin() {
