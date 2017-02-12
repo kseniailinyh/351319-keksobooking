b7c3cd3cc54bc053a5cfc0a748f11e423f6e06df
Ksenia Ilinyh <ksenia.ilinyh@gmail.com>
2017-02-12 10:47:18 +0500

    Полная изоляция, личный проект

--- js/form.js
+++ js/form.js
@@ -1,46 +1,49 @@
 'use strict';
 
-var price = document.querySelector('#price');
-var time = document.querySelector('#time');
-var timeout = document.querySelector('#timeout');
-var type = document.querySelector('#type');
-var roomNumber = document.querySelector('#room_number');
-var capacity = document.querySelector('#capacity');
-var title = document.querySelector('#title');
-var address = document.querySelector('#address');
+window.initializeForm = (function () {
+  var price = document.querySelector('#price');
+  var time = document.querySelector('#time');
+  var timeout = document.querySelector('#timeout');
+  var type = document.querySelector('#type');
+  var roomNumber = document.querySelector('#room_number');
+  var capacity = document.querySelector('#capacity');
+  var title = document.querySelector('#title');
+  var address = document.querySelector('#address');
 
-var timeValues = ['12', '13', '14'];
-var minPriceValues = ['0', '1000', '10000'];
-var typeValues = ['Лачуга', 'Квартира', 'Дворец'];
-var roomNumberValues = ['1', '2', '100'];
-var capacityValues = ['не для гостей', '3', '3'];
+  var timeValues = ['12', '13', '14'];
+  var minPriceValues = ['0', '1000', '10000'];
+  var typeValues = ['Лачуга', 'Квартира', 'Дворец'];
+  var roomNumberValues = ['1', '2', '100'];
+  var capacityValues = ['не для гостей', '3', '3'];
 
-var titleValidation = {
-  required: true,
-  minLength: 30,
-  maxLength: 100
-};
-var priceValidation = {
-  required: true,
-  min: 1000,
-  max: 1000000
-};
-var addressValidation = {
-  required: true
-};
+  var titleValidation = {
+    required: true,
+    minLength: 30,
+    maxLength: 100
+  };
+  var priceValidation = {
+    required: true,
+    min: 1000,
+    max: 1000000
+  };
+  var addressValidation = {
+    required: true
+  };
 
-// навешиваем валидацию на заголовок, цену и адрес
-window.setValidationRules(title, titleValidation);
-window.setValidationRules(price, priceValidation);
-window.setValidationRules(address, addressValidation);
+  // навешиваем валидацию на заголовок, цену и адрес
+  window.setValidationRules(title, titleValidation);
+  window.setValidationRules(price, priceValidation);
+  window.setValidationRules(address, addressValidation);
 
-// синхронизируем время заезда и выезда
-window.synchronizeFields(time, timeout, timeValues, timeValues, 'value');
-window.synchronizeFields(timeout, time, timeValues, timeValues, 'value');
+  // синхронизируем время заезда и выезда
+  window.synchronizeFields(time, timeout, timeValues, timeValues, 'value');
+  window.synchronizeFields(timeout, time, timeValues, timeValues, 'value');
 
-// синхронизируем количество комнат и количество мест
-window.synchronizeFields(roomNumber, capacity, roomNumberValues, capacityValues, 'value');
-window.synchronizeFields(capacity, roomNumber, capacityValues, roomNumberValues, 'value');
+  // синхронизируем количество комнат и количество мест
+  window.synchronizeFields(roomNumber, capacity, roomNumberValues, capacityValues, 'value');
+  window.synchronizeFields(capacity, roomNumber, capacityValues, roomNumberValues, 'value');
 
-// синхронизируем тип жилья с минимальной ценой
-window.synchronizeFields(type, price, typeValues, minPriceValues, 'min');
+  // синхронизируем тип жилья с минимальной ценой
+  window.synchronizeFields(type, price, typeValues, minPriceValues, 'min');
+
+})();
