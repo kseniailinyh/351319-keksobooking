d164de64e22aa10d5d6c0881a26208f982e4c812
Ksenia Ilinyh <ksenia.ilinyh@gmail.com>
2017-02-17 11:31:38 +0500

    Правки по ревью

--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -3,28 +3,82 @@
 window.initializePins = (function () {
   var map = document.querySelector('.tokyo__pin-map');
   var dialogClose = document.querySelector('.dialog__close');
+  var pins = document.querySelectorAll('.pin');
+  var dialog = document.querySelector('.dialog');
+  var ACTIVE_PIN_CLASS_NAME = 'pin--active';
+  var DIALOG_VISIBILITY_CLASS_NAME = 'invisible';
+
+  // активируем пин
+  function activatePin(evt, map, onCardClose) {
+    var target = evt.target;
+
+    while (target !== map) {
+      if (target.classList.contains('pin')) {
+        removeActivePin();
+        window.showCard(dialog, DIALOG_VISIBILITY_CLASS_NAME, deactivatePinByEsc);
+        target.classList.add(ACTIVE_PIN_CLASS_NAME);
+        target.blur();
+        if (typeof onCardClose === 'function') {
+          onCardClose(target);
+        }
+        return;
+      }
+      target = target.parentNode;
+    }
+  }
+
+  // деактивируем пин и закрываем диалог
+  function deactivatePin() {
+    removeActivePin();
+    hideDialog();
+  }
+
+  // делаем пины неактивными
+  function removeActivePin() {
+    for (var i = 0; i < pins.length; i++) {
+      pins[i].classList.remove(ACTIVE_PIN_CLASS_NAME);
+    }
+  }
+
+  // прячем диалог, плюс убираем событие по нажатию esc
+  function hideDialog() {
+    dialog.classList.add(DIALOG_VISIBILITY_CLASS_NAME);
+    document.removeEventListener('keydown', deactivatePinByEsc);
+  }
+
+  // если нажали esc, закрываем диалог
+  function deactivatePinByEsc(evt) {
+    if (window.utils.isEscKeyCode(evt)) {
+      deactivatePin();
+    }
+  }
+
+  // ставим фокус на элемент
+  function focusOpenButton (element) {
+    element.focus();
+  }
 
   // навешиваем событие по клику на карту
   map.addEventListener('click', function (evt) {
-    window.togglePins.activatePin(evt, map);
+    activatePin(evt, map);
   });
 
-  // навешиваем событие по нажатию enter на карту
+  // навешиваем событие по нажатию enter на карту, возвращаем фокус
   map.addEventListener('keydown', function (evt) {
     if (window.utils.isEnterKeyCode(evt)) {
-      window.togglePins.activatePin(evt, map);
+      activatePin(evt, map, focusOpenButton);
     }
   });
 
   // навешиваем событие по клику на крестик диалога
   dialogClose.addEventListener('click', function (evt) {
-    window.togglePins.deactivatePin();
+    deactivatePin();
   });
 
   // навешиваем событие по нажатию enter на крестик диалога, когда он в фокусе
   dialogClose.addEventListener('keydown', function (evt) {
     if (window.utils.isEnterKeyCode(evt)) {
-      window.togglePins.deactivatePin();
+      deactivatePin();
     }
   });
 
--- js/show-card.js
+++ js/show-card.js
@@ -1,32 +1,7 @@
 'use strict';
 
-window.showCard = (function () {
-  var dialog = document.querySelector('.dialog');
-  var DIALOG_VISIBILITY_CLASS_NAME = 'invisible';
-
-  // показываем диалог, плюс навешиваем событие по нажатию esc
-  function showDialog() {
-    dialog.classList.remove(DIALOG_VISIBILITY_CLASS_NAME);
-    document.addEventListener('keydown', deactivatePinByEsc);
-  }
-
-  // прячем диалог, плюс убираем событие по нажатию esc
-  function hideDialog() {
-    dialog.classList.add(DIALOG_VISIBILITY_CLASS_NAME);
-    document.removeEventListener('keydown', deactivatePinByEsc);
-  }
-
-  // если нажали esc, закрываем диалог
-  function deactivatePinByEsc(evt) {
-    if (window.utils.isEscKeyCode(evt)) {
-      window.togglePins.deactivatePin();
-    }
-  }
-
-  return {
-    showDialog: showDialog,
-    hideDialog: hideDialog,
-    deactivatePinByEsc: deactivatePinByEsc
-  };
-
-})();
+window.showCard = function (element, className, func) {
+  // удаляем у элемента класс и навешиваем событие по нажатию клавиши
+  element.classList.remove(className);
+  document.addEventListener('keydown', func);
+};
